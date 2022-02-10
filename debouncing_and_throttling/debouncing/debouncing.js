bccaa18955b3a0b5f5c26ef1d9779f02938f6216
Ravi Raj <raviraj14796@gmail.com>
2022-02-10 08:48:20 +0530

    debounce and throttle

--- debouncing_and_throttling/debouncing/debouncing.js
+++ debouncing_and_throttling/debouncing/debouncing.js
@@ -1,6 +1,4 @@
-//Function calls depends on user in debouncing
-
-counter = 0;
+let counter = 0;
 const getData = () => {
     //calls an api and gets data
     console.log('Fetching Data...', counter++)
@@ -11,18 +9,20 @@ const debounce = (fn, delay) => {
     return function () {
         let context = this,
             args = arguments;
-        console.log(context,args)
+        // console.log(context,args)
         clearTimeout(timer);
-        setTimeout(() => {
+        timer = setTimeout(() => {
             //console.log('inside timer')
             fn.apply(context, args)
         }, delay);
     }
 }
+const betterFunction = debounce(getData, 300);
 
 const searchBox = document.querySelector('.searchBox')
 searchBox.addEventListener('keyup', () => {
-    debounce(getData, 300)();
+    betterFunction();
+    // debounce(getData, 300)()  // if we use this way instead of storing into betterFunction, debouncing won't work as on every key stroke debounce() will give new reference.
 })
 
 //Some Examples :
--- debouncing_and_throttling/throttling/throttling.js
+++ debouncing_and_throttling/throttling/throttling.js
@@ -1,2 +1,29 @@
-//Function calls does not depend on user in throttling
+let counter = 0;
+const getData = () => {
+    //calls an api and gets data
+    console.log('Fetching Data...', counter++)
+}
 
+const throttle = function (fn, limit) {
+    let flag = true;
+    return function () {
+        let context = this,
+            args = arguments;
+        if (flag) {
+            fn.apply(context, args);
+            flag = false;
+            setTimeout(() => {
+                flag = true;
+            }, limit)
+        }
+    }
+}
+
+
+const betterFunction = throttle(getData, 300);
+
+const searchBox = document.querySelector('.searchBox');
+searchBox.addEventListener('keyup', () => {
+    betterFunction();
+    // throttle(getData, 300)()  // if we use this way instead of storing into betterFunction, throttlling won't work as on every key stroke throttle() will give new reference.
+})
